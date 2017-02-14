2bf4b570dcabd6f3a97546add3aaae4273291c76
Eliza Weisman <hi@hawkweisman.me>
2017-02-14 11:45:24 -0500

    better weights for models

--- karl_markov.py
+++ karl_markov.py
@@ -9,14 +9,25 @@ def make_karl():
     # Get raw text as string.
     with open("communist_manifesto.txt") as f:
         manifesto_text = f.read()
-    # skip the Project Gutenberg footer at the end
-    gutenberg_footer = 72910
-    # make the model
-    manifesto = markovify.Text(manifesto_text[:gutenberg_footer])
+
     with open("kapital_vol_1.txt") as f:
         kapital_text = cite_re.sub("", f.read())
+
+    # skip the Project Gutenberg footer at the end
+    gutenberg_footer = 72910
+    manifesto_text = manifesto_text[:gutenberg_footer]
+
+    # make the models for each input corpus
+    manifesto = markovify.Text(manifesto_text)
     kapital = markovify.Text(kapital_text)
-    return markovify.combine([manifesto, kapital])
+
+    # combine the models
+    manifesto_weight = 1
+    kapital_weight = kapital_text.len() / manifesto_text.len()
+    print("Das Kapital weight: {}".format(kapital_weight))
+
+    return markovify.combine([manifesto, kapital],
+                             [manifesto_weight, kapital_weight])
 
 com = make_karl()
 sentence = None
