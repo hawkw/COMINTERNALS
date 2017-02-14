5e44a510b8a9b29ab9c7dbe570e1f56069715b72
Eliza Weisman <hi@hawkweisman.me>
2017-02-14 11:53:22 -0500

    remove garbage text from files before making model

--- karl_markov.py
+++ karl_markov.py
@@ -3,15 +3,28 @@ import textwrap
 import re
 
 cite_re = re.compile(r"""((?:[0-9]+\. c,)?\s*(?:pp?\. [0-9]+\.?))""")
+hyphen_newline_re = re.compile(r"""(-$)""", re.MULTILINE)
+
+def dehyphenate(string):
+    """Remove hyphenated linebreaks from 'string'."""
+    return hyphen_newline_re.sub("", string)
+
+def preprocess(string):
+    """Remove hyphenated linebreaks and citations from 'string'."""
+    string = cite_re.sub("", string)
+    string = dehyphenate(string)
+    return string
+
+def load_file(path):
+    """Load a text file for making a model."""
+    with open(path) as f:
+        return preprocess(f.read())
 
 def make_karl():
     """Makes the Karl Markov model"""
     # Get raw text as string.
-    with open("communist_manifesto.txt") as f:
-        manifesto_text = f.read()
-
-    with open("kapital_vol_1.txt") as f:
-        kapital_text = cite_re.sub("", f.read())
+    manifesto_text = load_file("communist_manifesto.txt")
+    kapital_text = load_file("kapital_vol_1.txt")
 
     # skip the Project Gutenberg footer at the end
     gutenberg_footer = 72910
@@ -23,7 +36,7 @@ def make_karl():
 
     # combine the models
     manifesto_weight = 1
-    kapital_weight = kapital_text.len() / manifesto_text.len()
+    kapital_weight = len(manifesto_text) / len(kapital_text)
     print("Das Kapital weight: {}".format(kapital_weight))
 
     return markovify.combine([manifesto, kapital],
